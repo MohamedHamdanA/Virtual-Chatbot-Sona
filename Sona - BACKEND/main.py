8cafb2c24f46f538bafa27498c9bf70843a41cce
Addhithya <144675344+Addhithya@users.noreply.github.com>
2024-08-11 18:40:32 +0530

    Update main.py

--- Sona - BACKEND/main.py
+++ Sona - BACKEND/main.py
@@ -15,7 +15,7 @@ async def generate(prompt: Prompt):
     try:
         response = client.chat(
             model='llama3.1',
-            messages=[{'role': 'user', 'content': "text just for the prompt briefly as paragraph and without markdown :"+prompt.prompt }]
+            messages=[{'role': 'user', 'content': "You will always reply with a JSON array curly brackets of messages without any special characters. With a maximum of 3 messages but not necessarily 3 messages everytime .Each message has a text, facialExpression, and animation property.The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.The different animations are: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified, and Angry."+prompt.prompt }]
         )
         return {"text": response['message']['content']}
     except Exception as e:
